72c951f4d394ca63e4d83c1ee48deb2241fff0e1
Aronas72 <aronas.bagdevicius@mif.stud.vu.lt>
2025-09-05 12:13:07 +0300

    Pirma dalis, kuri nuskaito vartotojų duomenis, esančius struktūroje, suskaičiuoja balą pagal formulę ir išveda į ekraną rezultataus.

---
+++ lab/lab/labApp.swift
@@ -0,0 +1,17 @@
+//
+//  labApp.swift
+//  lab
+//
+//  Created by Aronas Bagdevičius on 15/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct labApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
