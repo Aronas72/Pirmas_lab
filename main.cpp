fcfe409b309d62ee20a9b04513514c3fce98b2dc
Aronas72 <aronas.bagdevicius@mif.stud.vu.lt>
2025-10-02 23:53:37 +0300

    Paskutinis atnaujinimas - sparta
    Paskutinis spartos atnaujinimas: galima pasirinkti kurio nori failo, iš naujai sukurtų, spartos matavimą

--- main.cpp
+++ main.cpp
@@ -3,95 +3,72 @@
 
 int main(){
     random_device rd;
-    mt19937 gener(rd());
-    uniform_int_distribution<int> pazym(1,10);
-    uniform_int_distribution<int> nd(1,10);
-    
-    cout<<"Pasirinkti veiksma:"<<endl;
-    cout<<"1 - ivesti studentus ranka arba nuskaityti is failo."<<endl;
-    cout<<"2 - sugeneruoti failus paciam"<<endl;
-    cout<<"Pasirinkimas: ";
-    int pasirinkimas=0;
-    cin>>pasirinkimas;
-    
-    double bendras_laikas=0.0;
-    
-    if(pasirinkimas==2){
-        {auto start=std::chrono::high_resolution_clock::now();
+        mt19937 gener(rd());
+        uniform_int_distribution<int> pazym(1,10);
+        uniform_int_distribution<int> nd(1,10);
+        
+        cout<<"Pasirinkti veiksma:"<<endl;
+        cout<<"1 - ivesti studentus ranka arba nuskaityti is failo."<<endl;
+        cout<<"2 - sugeneruoti failus paciam"<<endl;
+        cout<<"3 - atlikti testavima su sugeneruotais failais"<<endl;
+        cout<<"Pasirinkimas: ";
+        int pasirinkimas=0; cin>>pasirinkimas;
+        
+        if(pasirinkimas==2){
+            auto start=high_resolution_clock::now();
             generavimas("mano1000.txt", 1000, gener);
-            auto end=std::chrono::high_resolution_clock::now();
-            std::chrono::duration<double> diff=end-start;
-            cout<<"Failo mano1000.txt generavimas uztruko: "<<diff.count()<<" sekundziu"<<endl;
-            bendras_laikas+=diff.count();}
-        
-        {auto start=std::chrono::high_resolution_clock::now();
+            auto end=high_resolution_clock::now();
+            cout<<"Failo mano1000.txt generavimas uztruko: "<<duration<double>(end-start).count()<<" sekundziu"<<endl;
+            start=high_resolution_clock::now();
             generavimas("mano10000.txt", 10000, gener);
-            auto end=std::chrono::high_resolution_clock::now();
-            std::chrono::duration<double> diff=end-start;
-            cout<<"Failo mano10000.txt generavimas uztruko: "<<diff.count()<<" sekundziu"<<endl;
-            bendras_laikas+=diff.count();}
-        
-        {auto start=std::chrono::high_resolution_clock::now();
+            end=high_resolution_clock::now();
+            cout<<"Failo mano10000.txt generavimas uztruko: "<<duration<double>(end-start).count()<<" sekundziu"<<endl;
+            start=high_resolution_clock::now();
             generavimas("mano100000.txt", 100000, gener);
-            auto end=std::chrono::high_resolution_clock::now();
-            std::chrono::duration<double> diff=end-start;
-            cout<<"Failo mano100000.txt generavimas uztruko: "<<diff.count()<<" sekundziu"<<endl;
-            bendras_laikas+=diff.count();}
-        
-        {auto start=std::chrono::high_resolution_clock::now();
+            end=high_resolution_clock::now();
+            cout<<"Failo mano100000.txt generavimas uztruko: "<<duration<double>(end-start).count()<<" sekundziu"<<endl;
+            start=high_resolution_clock::now();
             generavimas("mano1000000.txt", 1000000, gener);
-            auto end=std::chrono::high_resolution_clock::now();
-            std::chrono::duration<double> diff=end-start;
-            cout<<"Failo mano1000000.txt generavimas uztruko: "<<diff.count()<<" sekundziu"<<endl;
-            bendras_laikas+=diff.count();}
-        
-        {auto start=std::chrono::high_resolution_clock::now();
+            end=high_resolution_clock::now();
+            cout<<"Failo mano1000000.txt generavimas uztruko: "<<duration<double>(end-start).count()<<" sekundziu"<<endl;
+            start=high_resolution_clock::now();
             generavimas("mano10000000.txt", 10000000, gener);
-            auto end=std::chrono::high_resolution_clock::now();
-            std::chrono::duration<double> diff=end-start;
-            cout<<"Failo mano10000000.txt generavimas uztruko: "<<diff.count()<<" sekundziu"<<endl;
-            bendras_laikas+=diff.count();}
+            end=high_resolution_clock::now();
+            cout<<"Failo mano10000000.txt generavimas uztruko: "<<duration<double>(end-start).count()<<" sekundziu"<<endl;
+        }
         
-        cout<<"------------------------------------------"<<endl;
-        cout<<"Bendras visu failu generavimo laikas: "<<bendras_laikas<<" sekundziu"<<endl;
-    
-    int pasirink=0;
-    cout<<"Ar nori testi programa?"<<endl;
-    cout<<"1 - baigti programa tik sugeneravus failus"<<endl;
-    cout<<"2 - testi toliau (duomenu nuskaitymas/ivedimas)"<<endl;
-    cout<<"Pasirinkimas: ";
-    cin>>pasirink;
-    
-    if(pasirink == 1){
-        cout<<"Programa baigta."<<endl;
-        return 0;}}
-    
-    vector<Studentas> grupe=nuskaitymas(gener, pazym, nd);
-    
-    int pasirinkti=0;
-    cout<<"Pasirinkite galutinio balo skaiciavimo buda:"<<endl;
-    cout<<"1 - tik vidurkis"<<endl;
-    cout<<"2 - tik mediana"<<endl;
-    cout<<"3 - abu"<<endl;
-    cout<<"Jusu pasirinkimas: ";
-    cin>>pasirinkti;
-    
-    int rik;
-    cout<<"Pasirinkti pagal ka rikiuoti:"<<endl;
-    cout<<"1 - pagal varda"<<endl;
-    cout<<"2 - pagal pavarde"<<endl;
-    cout<<"3 - pagal galutini bala"<<endl;
-    cout<<"Pasirinkimas: ";
-    cin>>rik;
-    
-    vector<Studentas> vargsiukai;
-    vector<Studentas> galvociai;
-    
-    for(const auto & st: grupe){
-        double balas = (pasirinkti == 2 ? st.med : st.gal);
-        if(balas < 5.0) vargsiukai.push_back(st);
-        else galvociai.push_back(st);}
-    
-    isvedimas(grupe, vargsiukai, galvociai, pasirinkti, rik);
-}
+        if(pasirinkimas==3){
+            testavimas(gener, pazym, nd);
+            return 0;
+        }
+
+        vector<Studentas> grupe=nuskaitymas(gener, pazym, nd);
+        
+        int pasirinkti=0;
+        cout<<"Pasirinkite galutinio balo skaiciavimo buda:"<<endl;
+        cout<<"1 - tik vidurkis"<<endl;
+        cout<<"2 - tik mediana"<<endl;
+        cout<<"3 - abu"<<endl;
+        cout<<"Jusu pasirinkimas: ";
+        cin>>pasirinkti;
+        
+        int rik;
+        cout<<"Pasirinkti pagal ka rikiuoti:"<<endl;
+        cout<<"1 - pagal varda"<<endl;
+        cout<<"2 - pagal pavarde"<<endl;
+        cout<<"3 - pagal galutini bala"<<endl;
+        cout<<"Pasirinkimas: ";
+        cin>>rik;
+        
+        vector<Studentas> vargsiukai;
+        vector<Studentas> galvociai;
+        
+        for(const auto & st: grupe){
+            double balas = (pasirinkti == 2 ? st.med : st.gal);
+            if(balas < 5.0) vargsiukai.push_back(st);
+            else galvociai.push_back(st);}
+        
+        isvedimas(grupe, vargsiukai, galvociai, pasirinkti, rik);
+    }
+
 
